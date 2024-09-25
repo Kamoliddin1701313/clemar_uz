b49e57854cc93f5534eee3a464c9a2588761fdaf
Kamoliddin1701313 <kamol170professor@gmail.com>
2024-09-25 18:01:29 +0500

    clemar-uz tayyorlab bo'lindi

--- src/components/mainlayout/MainLayouts.jsx
+++ src/components/mainlayout/MainLayouts.jsx
@@ -3,14 +3,11 @@ import LargePictureCarousel from "../largepicturecarousel/LargePictureCarousel";
 import Categories from "../../pages/categories/Categories";
 import Product from "../../pages/product/Product";
 import BrandSponsorsCarousel from "../brandsponsorscarousel/BrandSponsorsCarousel";
-import OurNews from "../OurNews/OurNews";
+import OurNews from "../ournews/OurNews";
 import ContactUs from "../contactus/ContactUs";
 // import ProductsLayout from "../productsLayout/ProductsLayout";
 
 const MainLayouts = () => {
-  
-
-
   return (
     <div className="min-h-screen">
       <LargePictureCarousel />
